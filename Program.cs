3df152d6ef505094febfdbc4856b9716b9923e5b
rochagabs <gabirochasf@gmail.com>
2024-01-09 23:05:51 -0300

    Desafio DIO do Decola Tech concluídogit add DesafioPOO.csproj !

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia("999999999", "A0", "356303486094384", 16);
+n1.InstalarAplicativo("Candy Crush");
+n1.Ligar();
+n1.ReceberLigacao();
+
+Console.WriteLine("---------------------------------");
+Iphone i1 = new Iphone("988888888", "15 pro max", "356303487228718", 32);
+i1.InstalarAplicativo("Pou");
+i1.Ligar();
+i1.ReceberLigacao();
